83ff3a1d6f4299d44faad536c9b6e8bcbb3a6eea
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-21 19:55:34 -0300

    created 'Element' class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,29 @@
 package main;
 
 public class Main {
+    public static void main(String[] args) {
 
+    }
+
+    public class Element {
+
+        private int data;
+        private Element next;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Element getNext() {
+            return next;
+        }
+
+        public void setNext(Element next) {
+            this.next = next;
+        }
+    }
 }
