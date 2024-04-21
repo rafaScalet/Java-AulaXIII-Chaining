ab08dbb6891d1fdac4a95dc89668eecda818a5e0
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-21 20:07:23 -0300

    alteration on 'element' class, and complete the system

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,43 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Element item;
+        Element top = null;
+        Element last = null;
+        int data = 0;
 
+        Scanner in = new Scanner(System.in);
+
+        while (data >= 0) {
+            System.out.print("Dado: ");
+            data = in.nextInt();
+            if (data < 0) {
+                continue;
+            }
+            item = new Element();
+            item.setData(data);
+            item.setNext(null);
+            if (top == null) {
+                top = item;
+                last = item;
+            } else {
+                last.setNext(item);
+                last = item;
+            }
+        }
+        in.close();
+
+        item = top;
+        while (item != null) {
+            System.out.println("Dado: " + item.getData());
+            item = item.getNext();
+        }
     }
 
-    public class Element {
+    public static class Element {
 
         private int data;
         private Element next;
